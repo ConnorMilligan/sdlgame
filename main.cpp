b2322a2b003468c2c211d87cbd66c7176653763f
Connor Milligan <connoramilligan@gmail.com>
2022-10-04 14:26:21 -0400

    Lesson 2

--- main.cpp
+++ main.cpp
@@ -4,34 +4,74 @@
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
+SDL_Window* gWindow = NULL;
+SDL_Surface* gScreenSurface = NULL;
+SDL_Surface* gHelloWorld = NULL;
+
+bool init();
+bool loadMedia();
+void close();
+
 int main(int argc, char** args) {
-	SDL_Window *window = NULL;
-	SDL_Surface *surface = NULL;
+	if (!init()) {
+		printf("Failed to initialize!\n");
+	}
+	else {
+		if(!loadMedia()) {
+			printf("Failed to load media!\n");
+		}
+		else {
+			SDL_BlitSurface(gHelloWorld, NULL, gScreenSurface, NULL);
+			SDL_UpdateWindowSurface(gWindow);
+			SDL_Delay(2000);
+		}
+	}
+	
+	close();
+	return 0;
+}
+
+bool init() {
+	bool success = true;
 
 	if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         printf("SDL could not initialize! SDL_Error: %s\n", SDL_GetError());
+		success = false;
 	}
 	else {
-		window = SDL_CreateWindow("SDL tutorial", 
-			SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 
-			SCREEN_WIDTH, SCREEN_WIDTH, SDL_WINDOW_SHOWN);
+		gWindow = SDL_CreateWindow("SDL tutorial", 
+				SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 
+				SCREEN_WIDTH, SCREEN_WIDTH, SDL_WINDOW_SHOWN);
 
-		if (window == NULL) {
+		if (gWindow == NULL) {
 			printf("Window could not be created! SDL_Error: %s\n", SDL_GetError());
+			success = false;
 		}
 		else {
-			surface = SDL_GetWindowSurface(window);
-
-			SDL_FillRect(surface, NULL, SDL_MapRGB(surface->format, 0xFF, 0xFF, 0xFF));
-			SDL_UpdateWindowSurface(window);
-
-			SDL_Delay(2000);
+			gScreenSurface = SDL_GetWindowSurface(gWindow);
 		}
-
-		SDL_DestroyWindow(window);
-		SDL_Quit();
-
-		return 0;
 	}
-	
+	return success;
+}
+
+bool loadMedia() {
+	bool success = true;
+
+	gHelloWorld = SDL_LoadBMP("hello_world.bmp");
+	if (gHelloWorld == NULL) {
+		printf("Unable to load image %s! SDL Error: %s\n", "hello_world.bmp", SDL_GetError());
+		success = false;
+	}
+
+	return success;
+}
+
+void close() {
+	SDL_FreeSurface(gHelloWorld);
+	gHelloWorld = NULL;
+
+	SDL_DestroyWindow(gWindow);
+	gWindow = NULL;
+
+	SDL_Quit();
 }
