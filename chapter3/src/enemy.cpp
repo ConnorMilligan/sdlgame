126972d4867032934dde9fd91cc6a633500cacb0
Connor Milligan <connoramilligan@gmail.com>
2023-04-01 12:22:28 -0400

    redoing with C

--- chapter3/src/enemy.cpp
+++
@@ -1,8 +0,0 @@
-#include "enemy.h"
-
-Enemy::Enemy(const LoaderParams *pParams) : SDLGameObject(pParams) {}
-
-void Enemy::update() {
-    this->m_x++;
-    this->m_currentFrame = int(((SDL_GetTicks() / 100) %6));
-}
--- chapter3/src/player.cpp
+++
@@ -1,8 +0,0 @@
-#include "player.h"
-
-Player::Player(const LoaderParams *pParams) : SDLGameObject(pParams) {}
-
-void Player::update() {
-    this->m_x--;
-    this->m_currentFrame = int(((SDL_GetTicks() / 100) %6));
-}
